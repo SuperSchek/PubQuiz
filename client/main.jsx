325f07c724294388a4393b27ca7ecc2d4453925e
SuperSchek <sanderschekman@gmail.com>
2016-04-28 22:01:20 +0300

    Removed some unused code.

--- client/main.jsx
+++ client/main.jsx
@@ -1,9 +1,6 @@
-import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { render } from 'react-dom';
 
 import './main.html';
-import 'routes.jsx';
 
 import {test} from '../db/main.js';
 
