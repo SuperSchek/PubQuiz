2dbb98d65c4f6673ec9f4d39d71049b60f2d1ecf
Sander Schekman <sanderschekman@gmail.com>
2016-04-28 16:19:14 +0200

    Added iron-router and some sample-routes

--- client/main.jsx
+++ client/main.jsx
@@ -2,5 +2,13 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
+import 'routes.jsx';
 
 import {test} from '../db/main.js';
+
+animateContentOut = function() {
+    $('#one').removeClass("animated fadeIn");
+    return $('footer').addClass("hide");
+};
+
+// Router.onBeforeAction(animateContentOut);
--- server/kadira.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Sander on 26/04/16.
- */
-
-Kadira.connect('cGuxDZeTxfnfb9CwE', 'd5674d53-36d2-4fe1-ae00-e877fed7f9e9');
