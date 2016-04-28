4e35a0182ece888734d75f0794d6be293d000b93
Jacco Schumacher <jaccoschumacher@Jaccos-MacBook-Air.local>
2016-04-28 16:19:01 +0200

    iron-router added

--- client/main.jsx
+++ client/main.jsx
@@ -4,3 +4,18 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import './main.html';
 
 import {test} from '../db/main.js';
+
+Router.route('/', function () {
+    this.render('home');
+});
+
+Router.route('/one', function () {
+    this.render('one');
+});
+
+function fadeContentIn() {
+    document.getElementById('test').addClass("animated fadeIn");
+}
+
+// define this as a global onAfterAction so it happens all the time
+Router.onAfterAction(fadeContentIn);
