cdb2fdcbbf08d839aaba2fa5a71071c161556be3
Sander Schekman <sanderschekman@gmail.com>
2016-04-28 13:21:37 +0200

    Working on Meteor sample app with React

--- client/main.jsx
+++ client/main.jsx
@@ -1,6 +1,11 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { render } from 'react-dom';
 
-import './main.html';
+import App from '../imports/ui/App.jsx';
 
-import {test} from '../db/main.js';
+import {PubQuiz} from '../db/main.js';
+
+Meteor.startup(() => {
+    render(<App />, document.getElementById('render-target'));
+});
