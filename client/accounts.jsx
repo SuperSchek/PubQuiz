660717bb1553b80bb9d5cadaf14381fcbee4d832
Jorre Spijker <jorrespijker@MacBook-Pro-van-Jorre.local>
2016-05-12 11:55:52 +0200

    Finished login function

--- client/accounts.jsx
+++ client/accounts.jsx
@@ -2,3 +2,82 @@
  * Created by jorrespijker on 10-05-16.
  */
 
+Template.login.helpers({
+    errorMessage: function() {
+        return Session.get('errorMessage');
+    }
+});
+
+Template.register.events({
+    'submit form': function(event) {
+        event.preventDefault();
+        var emailVar = event.target.email.value;
+        var passwordVar = event.target.password.value;
+        Accounts.createUser({
+            email: emailVar,
+            password: passwordVar
+        }, function(error){
+            if(error){
+                console.log(error.reason); // Output error if registration fails
+            } else {
+                Router.go("lobby"); // User succeeds
+            }
+        });
+    }
+});
+
+Template.login.events({
+    'submit form': function(event){
+        event.preventDefault();
+        var emailVar = event.target.email.value;
+        var passwordVar = event.target.password.value;
+        Meteor.loginWithPassword(emailVar, passwordVar, function(error){
+            if (error) {
+                console.log(error.reason);
+            } else {
+                Router.go("lobby"); // User succeeds
+            }
+        });
+    }
+});
+
+Template.dashboard.events({
+    'click .logout': function(event){
+        event.preventDefault();
+        Meteor.logout();
+    }
+});
+
+
+// Error Handling
+
+$.validator.setDefaults({
+    rules: {
+        email: {
+            required: true,
+            email: true
+        },
+        password: {
+            required: true,
+            minlength: 6
+        }
+    },
+    messages: {
+        email: {
+            required: "You must enter an email address.",
+            email: "You've entered an invalid email address."
+        },
+        password: {
+            required: "You must enter a password.",
+            minlength: "Your password must be at least {0} characters."
+        }
+    }
+});
+
+Template.login.onRendered(function(){
+    $('.login').validate();
+});
+
+Template.register.onRendered(function(){
+    $('.register').validate();
+});
