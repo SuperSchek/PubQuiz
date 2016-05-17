963cefa1e889a77289b6b47d23e334da19cf0dcd
Sander Schekman <sanderschekman@gmail.com>
2016-05-17 13:56:18 +0200

    Moved QuizQuestions.upsert from client to server startup.

--- server/main.jsx
+++ server/main.jsx
@@ -7,6 +7,37 @@ Meteor.startup(() => {
     });
 
     QuizQuestions = new Mongo.Collection("quiz");
+
+    QuizQuestions.upsert(
+        {
+            question: "Wat is de vijfde planeet in ons zonnestelsel gemeten vanaf de zon?",
+            answers: [
+                {
+                    answer: "Venus",
+                    punten: false,
+                    rendered: false
+                },
+                {
+                    answer: "Mars",
+                    punten: false,
+                    rendered: false
+                },
+                {
+                    answer: "Saturnus",
+                    punten: false,
+                    rendered: false
+                },
+                {
+                    answer: "Jupiter",
+                    uitleg: '<img src="../images/zonnestelsel.png" width="100%"/>',
+                    punten: true,
+                    rendered: false
+                }
+            ],
+            picture: "<img src='../images/stars.png' width='100%'/>",
+            enabled:true
+        }
+    );
 });
 
 Meteor.methods({
