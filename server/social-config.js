49b8b9c00a0c9083a5b9a0a7d68266c93af831ac
Sander Schekman <sanderschekman@gmail.com>
2016-05-24 11:05:41 +0200

    Added Facebook login credentials for Heroku domain.

--- server/social-config.js
+++ server/social-config.js
@@ -8,6 +8,6 @@ ServiceConfiguration.configurations.remove({
 
 ServiceConfiguration.configurations.insert({
     service: "facebook",
-    appId: "1006642112763779",
-    secret: "c22ea768eaaa60f40fb6e31a18a626db"
+    appId: "1006642112763779" || "1006641632763827",
+    secret: "c22ea768eaaa60f40fb6e31a18a626db" || "265e4362c0f9a49ff947fe83314a81cb"
 });
