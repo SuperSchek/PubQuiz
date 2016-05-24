4ae358f7c86d00cfe03ef876147ba1e325c290a8
Sander Schekman <sanderschekman@gmail.com>
2016-05-24 13:02:07 +0200

    Moved everything regarding the questions Collection to the server-side.

--- db/main.js
+++ db/main.js
@@ -2,4 +2,6 @@
  * Created by Sander on 26/04/16.
  */
 
+Channels = new Mongo.Collection("Channels");
+
 test = new Mongo.Collection("PubQuiz");
--- server/main.jsx
+++ server/main.jsx
@@ -1,16 +1,11 @@
 import { Meteor } from 'meteor/meteor';
-import { QuizQuestions } from '../db/quiz.js';
 
 Meteor.startup(() => {
-
-    // code to run on server at startup
     Accounts.config({
         forbidClientAccountCreation : false
     });
 });
 
 Meteor.methods({
-    triggered: function () {
-        console.log('Message received!');
-    }
+
 });
